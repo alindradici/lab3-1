392f08e413392eb05cf27db9eb33c3e8e0e3727f
Oana Balint <oanab95@gmail.com>
2016-03-26 13:07:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Salut Lume!");
+        System.out.println("Salut Lume 2!");
+        System.out.println("Salut Lume 3 !");
+        System.out.println("Salut Lume4!");
+        System.out.println("Salut Lume5!");
+
+    }
+}
